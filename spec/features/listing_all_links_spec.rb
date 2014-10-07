bc17839e4870d1bcc039eb4b39ce0ae23eb27e39
mala23 <mail@mala23.me>
2014-10-07 20:33:32 +0100

    Add functionality to filter links by tags

--- spec/features/listing_all_links_spec.rb
+++ spec/features/listing_all_links_spec.rb
@@ -1,15 +1,33 @@
 require 'spec_helper'
 
 feature "User browses the list of links" do
-	
-	before(:each) {
-		Link.create(:uri => "http://www.makersacademy.com",
-					:title => "Makers Academy")
-	}
 
+  before(:each) {
+	Link.create(:uri => "http://www.makersacademy.com",
+		:title => "Makers Academy",
+		:tags => [Tag.first_or_create(:text => 'education')])
+	Link.create(:uri => "http://www.google.com",
+		:title => "Google",
+		:tags => [Tag.first_or_create(:text => 'search')])
+	Link.create(:uri => "http://www.bing.com",
+		:title => "Bing",
+		:tags => [Tag.first_or_create(:text => 'search')])
+	Link.create(:uri => "http://www.code.org",
+		:title => "Code.org",
+		:tags => [Tag.first_or_create(:text => 'education')])
+	}
+	
 	scenario "land on the page" do
 		visit '/'
 		expect(page).to have_content("Makers Academy")
+	end
+
+	scenario "User filters links by a tag" do
+		visit '/tags/search'
+		expect(page).not_to have_content("MakersAcademy")
+		expect(page).not_to have_content("Code.org")
+		expect(page).to have_content("Google")
+		expect(page).to have_content("Bing")
 	end
 
 end
